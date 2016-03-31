1e6b4b63774844ea0a71f462209ccf8d2fe1f256
zd <zhendong9860@163.com>
2016-03-31 11:08:04 +0800

    matrix transition

--- slides/js/generalGeometry.js
+++ slides/js/generalGeometry.js
@@ -20,35 +20,43 @@ function circle (row){
 
   return　[pos,inx];
 }
-function Sphere(r, segments){
+function Sphere(r, row, column,color){
   let pos = [],nor = [], col = [], inx=[];
-  for(let i =0 ;i<segments+1;i++){
+  for(let i =0 ;i<column+1;i++){
     //经度
-    let a = Math.PI*2/segments*i;
+    let a = Math.PI*2/column*i;
     
     let ca = Math.cos(a);
     let sa = Math.sin(a);
-    for(let j = 0;j<segments+1;j++){
+    for(let j = 0;j<row+1;j++){
       //纬度
-      let b = Math.PI*2/segments*j;
+      let b = Math.PI*2/row*j;
       let cb = Math.cos(b);
       let sb = Math.sin(b);
       let x = r*cb*ca;
       let y = r*sb;
       let z = r*cb*sa;
+
+      if(color){
+        var tc = color;
+      }else{
+        tc = hsva(360/row*i,1,1,1);
+      }
+
       pos.push(x,y,z);
       nor.push(cb*ca,sb,cb*sa);
+      col.push(tc[0],tc[1],tc[2],tc[3]);
     }
   }
   //general segments
-  for(let i = 0;i<segments;i++){
-    for(let j = 0;j<segments;j++){
-      r = i*segments+j;
-      inx.push(r,r+segments,r+1);
-      inx.push(r+1,r+segments+1,r+segments);
+  for(let i = 0;i<column;i++){
+    for(let j = 0;j<row;j++){
+      r = i*row+j;
+      inx.push(r,r+row,r+1);
+      inx.push(r+1,r+row+1,r+row);
     }
   }
-  return [pos,inx];
+  return {pos:pos,nor:nor,col:col,inx:inx};
 }
 
 function torus(row, column, irad, orad){

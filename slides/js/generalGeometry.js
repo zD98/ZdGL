a1ac457978842feb1a10095d10e980cc296085d8
zd <zhendong9860@163.com>
2016-04-09 23:49:38 +0800

    matrix transition

--- slides/js/generalGeometry.js
+++ slides/js/generalGeometry.js
@@ -114,3 +114,33 @@ function torusN(row, column, irad, orad){
   }
   return [pos,nor,col, idx];
 }
+
+function torusNT(row, column, irad, orad){
+  var pos =[], tex = [],nor = [] ,idx = [];
+  for(var i = 0; i <= row; i++){
+    var r = Math.PI * 2 / row * i;
+    var rr = Math.cos(r);
+    var ry = Math.sin(r);
+    for(var ii = 0; ii <= column; ii++){
+      var tr = Math.PI * 2 / column * ii;
+      var tx = (rr * irad + orad) * Math.cos(tr);
+      var ty = ry * irad;
+      var tz = (rr * irad + orad) * Math.sin(tr);
+
+      var rx = rr * Math.cos(tr);
+      var rz = rr * Math.sin(tr);
+      pos.push(tx, ty, tz);
+      nor.push(rx,ry,rz);
+
+      tex.push(ii/row,i/column);
+    }
+  }
+  for(i = 0; i < row; i++){
+    for(ii = 0; ii < column; ii++){
+      r = (column + 1) * i + ii;
+      idx.push(r, r + column + 1, r + 1);
+      idx.push(r + column + 1, r + column + 2, r + 1);
+    }
+  }
+  return [pos,nor,tex, idx];
+}
